4dda5241b154d721ac9310576e0680edcf495471
Mm897xxc <mislav@kolegio.hr>
2025-05-26 11:12:56 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SavingsCalculator from './SavingsCalculator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SavingsCalculator />
+  </React.StrictMode>
+);
